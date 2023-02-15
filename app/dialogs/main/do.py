1f41d0faadb979cdfb22b89e85da9a60ca5ff502
Ilya Smirnov <ismirnov@aligntech.com>
2023-02-15 13:35:21 +0400

    fix: chat_id name

--- app/dialogs/main/do.py
+++ app/dialogs/main/do.py
@@ -6,7 +6,7 @@ from aiogram_dialog import DialogManager, StartMode
 from app.bot_loader import bot
 from app.dialogs.main.states import Main
 from app.dialogs.main.parsers import PostCardData
-from app.config import SEX_CHAT_ID
+from app.config import CHAT_ID
 from app.extensions.widgets import Button
 
 logger = logging.getLogger(__name__)
@@ -17,7 +17,7 @@ async def postcard_send(c: CallbackQuery, button: Button, dialog_manager: Dialog
     text = "\n".join(data.text) or None
 
     if len(data.photos):
-        await bot.send_photo(SEX_CHAT_ID, data.photos[0], text, has_spoiler=True)
+        await bot.send_photo(CHAT_ID, data.photos[0], text, has_spoiler=True)
     else:
         if text is None:
             await c.message.answer("Что-то пошло не так, "
@@ -25,6 +25,6 @@ async def postcard_send(c: CallbackQuery, button: Button, dialog_manager: Dialog
                                    "@mindsweeper")
             return await dialog_manager.start(Main.menu, mode=StartMode.RESET_STACK)
 
-        await bot.send_message(SEX_CHAT_ID, f"{text}")
+        await bot.send_message(CHAT_ID, f"{text}")
 
     await dialog_manager.start(Main.sent, mode=StartMode.RESET_STACK)
